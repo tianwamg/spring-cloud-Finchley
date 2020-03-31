97f044a25e35bef943d017d3fe9715efbc01a73e
wangpp <987897856@qq.com>
2020-03-31 10:36:51 +0800

    修改为json格式

--- common/src/main/java/com/cn/util/HttpRequestUtil.java
+++ common/src/main/java/com/cn/util/HttpRequestUtil.java
@@ -5,6 +5,7 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -12,6 +13,7 @@ import org.apache.http.message.BasicNameValuePair;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +32,7 @@ public class HttpRequestUtil {
      * @param params
      * @return
      */
-    public static String httpPostFormdata(String url, Map<String,String> params){
+    public static String httpPostFormdata(String url, String jsonStr){
         CloseableHttpResponse response = null;
         BufferedReader in = null;
         String result = "";
@@ -42,14 +44,15 @@ public class HttpRequestUtil {
             //设置header
             httpPost.setHeader("Content-Type","application/x-www-form-urlencoded");
             //组织请求参数
-            List<NameValuePair> list = new ArrayList<>();
+            /*List<NameValuePair> list = new ArrayList<>();
             if(params != null && params.size() >0){
                 Set<String> keySet = params.keySet();
                 for(String key:keySet){
                     list.add(new BasicNameValuePair(key,params.get(key)));
                 }
             }
-            httpPost.setEntity(new UrlEncodedFormEntity(list, "utf-8"));
+            httpPost.setEntity(new UrlEncodedFormEntity(list, "utf-8"));*/
+            httpPost.setEntity(new StringEntity(jsonStr, Charset.forName("UTF-8")));
             response = closeableHttpClient.execute(httpPost);
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             StringBuilder stringBuilder = new StringBuilder("");
