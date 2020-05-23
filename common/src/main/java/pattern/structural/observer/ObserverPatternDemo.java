eb50cbfb64c48bb0c07755cf25246607ff25117c
tianwamg <987897856@qq.com>
2020-05-23 22:36:30 +0800

    观察者模式

---
+++ common/src/main/java/pattern/structural/observer/ObserverPatternDemo.java
@@ -0,0 +1,19 @@
+package pattern.structural.observer;
+
+/**
+ * 观察者模式
+ */
+public class ObserverPatternDemo {
+
+    public static void main(String agrs[]){
+        Subject subject = new Subject();
+
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+        new HexObserver(subject);
+        System.out.println("first state change: 15");
+        subject.setState(15);
+        System.out.println("second state change: 10");
+        subject.setState(10);
+    }
+}
