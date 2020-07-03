d73679757ff1b9d91cd468e9c4769257b25d87cb
tianwamg <987897856@qq.com>
2020-07-04 00:01:48 +0800

    eureka-provider2 initial

---
+++ eureka-provider2/src/test/java/com/cn/eurekaprovider2/EurekaProvider2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.eurekaprovider2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaProvider2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
