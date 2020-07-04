3914482729c7448ca83955fb9839675c959f0aee
tianwamg <987897856@qq.com>
2020-07-04 16:36:14 +0800

    eureka-zuul initial

---
+++ eureka-zuul/src/test/java/com/zuul/eurekazuul/EurekaZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.eurekazuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
