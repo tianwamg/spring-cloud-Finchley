e91e40a9312c8f4c3e146d8667eb2f808b322b62
tianwamg <987897856@qq.com>
2020-07-05 16:05:54 +0800

    config eureka

--- config-server/src/main/java/com/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/configserver/ConfigServerApplication.java
@@ -2,9 +2,13 @@ package com.config.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
