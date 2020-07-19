1e7aa1ea7890d5d1db67dac4158a2d8e159eccea
tianwamg <987897856@qq.com>
2020-07-19 22:35:35 +0800

    zipkin

---
+++ sleuth-zipkin/src/test/java/com/sleuth/sleuthzipkin/SleuthZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleuth.sleuthzipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
